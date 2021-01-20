6818682ed1f4d2568fe41b9aa24b31d2e7df3ed1
Elena Topaka <el1topa@gmail.com>
2021-01-20 13:15:55 +0200

    Simple electron app with touchbar spin
    Simple electron app 'Hello world' with touchbar spin for macos

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const { getTouchbar } = require('./touchbar')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  if (process.platform === 'darwin') {
+    const touchBar = getTouchbar()
+    win.setTouchBar(touchBar)
+  }
+
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
